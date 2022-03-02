e2df5f16cccdda476fb5af2914161d56044cd8b5
lienzoenblanco <raquelgarciatrives@gmail.com>
2022-03-02 19:48:42 +0000

    feat(navbar): menu depending you are logged or not

--- src/front/js/service/base.js
+++ src/front/js/service/base.js
@@ -1,2 +1,2 @@
 export const BaseUrl =
-  "https://3001-lienzoenblanco-finalproy-n11lkq44c26.ws-eu34xl.gitpod.io/api";
+  "https://3001-lienzoenblanco-makemenue-ui3z6t478k8.ws-eu34xl.gitpod.io/api";
