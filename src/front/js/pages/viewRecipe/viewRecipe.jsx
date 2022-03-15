17d9fa162fa4a9a36c92bc72b8fb5dc7f7601d37
lienzoenblanco <raquelgarciatrives@gmail.com>
2022-03-15 20:50:45 +0000

    WIP: view one recipe

---
+++ src/front/js/pages/viewRecipe/viewRecipe.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useContext, useEffect } from "react";
+
+import "../viewRecipe/viewRecipe.css";
+
+export const ViewRecipe = () => {
+  return (
+    <div className="container">
+      <h3>Esto es una receta</h3>
+      <div className="row">
+        <div className="col"></div>
+        <div className="col"></div>
+      </div>
+    </div>
+  );
+};
