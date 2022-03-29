3a0f48dccee50c2589aae528808fa057d2b6f501
lienzoenblanco <raquelgarciatrives@gmail.com>
2022-03-29 16:06:27 +0000

    feat(menu):get and generate menu

--- src/front/js/pages/myMenus/myMenus.jsx
+++ src/front/js/pages/myMenus/myMenus.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../../store/appContext";
-import { Link } from "react-router-dom";
 
 import { getMenu, autoMenu } from "../../service/menu";
 
@@ -14,9 +13,11 @@ export const MyMenus = () => {
   const [loading, setLoading] = useState(false);
 
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [week, setWeek] = useState([]);
 
   useEffect(() => {
     setLoading(true);
+    setWeek(calculateWeek(currentDate));
     getMenu(currentDate.toISOString())
       .then((response) => response.json())
       .then((data) => {
@@ -24,11 +25,11 @@ export const MyMenus = () => {
           setLoading(false);
           setMenuNotFound(true);
         } else {
+          setLoading(false);
           setMenu(data);
         }
-        setMenu();
       });
-  }, []);
+  }, [currentDate]);
 
   const newMenu = () => {
     setLoading(true);
@@ -36,8 +37,77 @@ export const MyMenus = () => {
       .then((response) => response.json())
       .then((data) => {
         setMenu(data);
+        setMenuNotFound(false);
       });
     setLoading(false);
+  };
+
+  const capitalize = (word) => {
+    const lower = word.toLowerCase();
+    return word.charAt(0).toUpperCase() + lower.slice(1);
+  };
+
+  const getMonday = (date) => {
+    date = new Date(date);
+    let day = date.getDay(),
+      diff = date.getDate() - day + (day == 0 ? -6 : 1);
+    return new Date(date.setDate(diff));
+  };
+
+  const calculateWeek = (date) => {
+    const monday = getMonday(date);
+    let week = [monday];
+    for (let index = 1; index < 7; index++) {
+      week.push(
+        new Date(
+          monday.getFullYear(),
+          monday.getMonth(),
+          monday.getDate() + index
+        )
+      );
+    }
+    return week;
+  };
+
+  const previousWeek = () => {
+    setCurrentDate(
+      new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        currentDate.getDate() - 7
+      )
+    );
+  };
+
+  const nextWeek = () => {
+    setCurrentDate(
+      new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        currentDate.getDate() + 7
+      )
+    );
+  };
+
+  const getLunchList = () => {
+    let lunchList = [];
+    if ("menu_recipe_list" in menu) {
+      lunchList = menu.menu_recipe_list.filter((menu_recipe) => {
+        return menu_recipe.selected_tag == 1;
+      });
+    }
+
+    return lunchList;
+  };
+
+  const getDinnerList = () => {
+    let dinnerList = [];
+    if ("menu_recipe_list" in menu) {
+      dinnerList = menu.menu_recipe_list.filter((menu_recipe) => {
+        return menu_recipe.selected_tag == 2;
+      });
+    }
+    return dinnerList;
   };
 
   return (
@@ -48,45 +118,89 @@ export const MyMenus = () => {
             Generar menú
           </button>
           <div className="alert alert-light" role="alert">
-            No tienes menú para esta semana. Clicka en Generar menú para
-            crearlo.
+            No tienes menú asignado para esta semana. Clicka en Generar menú
+            para crearlo.
           </div>
         </section>
       ) : (
-        <div className="table-responsive-lg">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Lunes</th>
-                <th>Martes</th>
-                <th>Miércoles</th>
-                <th>Jueves</th>
-                <th>Viernes</th>
-                <th>Sábado</th>
-                <th>Domingo</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>Jacob</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                <td>@fat</td>
-                <td>@fat</td>
-                <td>@fat</td>
-              </tr>
-            </tbody>
-          </table>
+        <div>
+          <section className="move-weeks">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-arrow-left"
+              viewBox="0 0 16 16"
+              onClick={previousWeek}
+            >
+              <path
+                fill-rule="evenodd"
+                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-arrow-right"
+              viewBox="0 0 16 16"
+              onClick={nextWeek}
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+              />
+            </svg>
+
+            <div>
+              <label className="month">
+                {capitalize(
+                  currentDate.toLocaleDateString("es-es", {
+                    month: "long",
+                    year: "numeric",
+                  })
+                )}
+              </label>
+            </div>
+          </section>
+          <div className="table-responsive-lg">
+            <table className="table">
+              <thead>
+                <tr>
+                  {week.map((day, i) => {
+                    return (
+                      <th key={i}>
+                        {capitalize(
+                          day.toLocaleDateString("es-es", {
+                            weekday: "long",
+                            day: "numeric",
+                          })
+                        )}
+                      </th>
+                    );
+                  })}
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  {getLunchList().map((menu_recipe) => {
+                    return (
+                      <td key={menu_recipe.id}>{menu_recipe.recipe.title}</td>
+                    );
+                  })}
+                </tr>
+                <tr>
+                  {getDinnerList().map((menu_recipe) => {
+                    return (
+                      <td key={menu_recipe.id}>{menu_recipe.recipe.title}</td>
+                    );
+                  })}
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
     </div>
